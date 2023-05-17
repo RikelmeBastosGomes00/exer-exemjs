e896ac9dd73791a27dde9a09b357f6b1ae0380c3
RikelmeBastosGomes00 <124384264+RikelmeBastosGomes00@users.noreply.github.com>
2023-05-17 08:41:20 -0300

    Add files via upload

---
+++ acoes.js
@@ -0,0 +1,16 @@
+function ac1(){
+    let resp = window.document.getElementById('saida')
+    resp.innerHTML += `<p>Clicou no primeiro botão</p>`
+}
+function ac2(){
+    let resp = window.document.getElementById('saida')
+    resp.innerHTML += '<p>Clicou no segundo botão</p>'
+}
+function ac3(){
+    let resp = window.document.getElementById('saida')
+    resp.innerHTML += '<p>Clicou no terceiro botão</p>'
+}
+function ac4(){
+    let resp = window.document.getElementById('saida')
+    resp.innerHTML += '<p>Clicou no quarto botão</p>'
+}
